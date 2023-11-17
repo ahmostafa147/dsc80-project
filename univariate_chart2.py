7cad5934ff615fcc0736fa96aa581d70e59e8dce
etvoucsd <129786988+etvoucsd@users.noreply.github.com>
2023-11-16 22:44:41 -0800

    Create univariate_chart2.py

---
+++ univariate_chart2.py
@@ -0,0 +1,8 @@
+fig = (px.histogram(lol_data,
+                    x="kills",
+                    facet_col= "position",
+                    facet_row = "result",
+                    title="Kills Distribution Per Position",
+                    histnorm="probability density")
+      )
+fig.show()
